d1a35a26a0bdd87738eca0f1c06ebc0befc828ea
louiszawadzki <louisz@bam.tech>
2021-01-15 18:24:28 +0100

    feat: 🎸 First failing test

--- Bowling.test.js
+++ Bowling.test.js
@@ -1,5 +1,15 @@
 const Bowling = require("./bowling");
 const { describe, it } = require("./best");
+
 describe("Bowling", () => {
-  it("handles a very boring game", () => {});
+  it("handles a very boring game", () => {
+    const game = new Bowling();
+    for (rollIndex = 0; rollIndex < 20; rollIndex++) {
+      game.roll(0);
+    }
+
+    if (game.getFinalScore() !== 0) {
+      throw new Error("not good");
+    }
+  });
 });
--- best.js
+++ best.js
@@ -1,4 +1,10 @@
-const describe = console.log;
+const describe = (title, callback) => {
+  try {
+    callback();
+  } catch (error) {
+    throw new Error(`${title} ${error.message}`);
+  }
+};
 
 module.exports = {
   describe,
