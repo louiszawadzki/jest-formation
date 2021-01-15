4d34b78713b25b206f250a2365b70ae5ef984757
louiszawadzki <louisz@bam.tech>
2021-01-15 18:27:49 +0100

    feat: 🎸 Make test message clearer

--- best.js
+++ best.js
@@ -1,12 +1,23 @@
 const describe = (title, callback) => {
   try {
     callback();
+    console.info(`All tests passed!`);
+    process.exit(0);
   } catch (error) {
-    throw new Error(`${title} ${error.message}`);
+    console.error(`Test failed! \n\n${title}\n ${error.message}`);
+    process.exit(1);
+  }
+};
+
+const it = (title, callback) => {
+  try {
+    callback();
+  } catch (error) {
+    throw new Error(`❌ ${title}: \n\nreceived: ${error.message}`);
   }
 };
 
 module.exports = {
   describe,
-  it: describe,
+  it,
 };
