5cae46e2acbc237f67d906466e87f21421f8a6e9
louiszawadzki <louisz@bam.tech>
2021-01-15 18:48:09 +0100

    feat: 🎸 monkey-patch util

--- Bowling.test.js
+++ Bowling.test.js
@@ -1,8 +1,10 @@
 const Bowling = require("./bowling");
 const { describe, it, expect } = require("./best");
+const utils = require("./getRandomRoll");
 
 describe("Bowling", () => {
   it("handles a very boring game", () => {
+    utils.getRandomRoll = () => 0;
     const game = new Bowling();
     for (rollIndex = 0; rollIndex < 20; rollIndex++) {
       game.roll(0);
@@ -12,6 +14,7 @@ describe("Bowling", () => {
   });
 
   it("handles a game with no spare no strike", () => {
+    utils.getRandomRoll = () => 1;
     const game = new Bowling();
     for (rollIndex = 0; rollIndex < 20; rollIndex++) {
       game.roll(1);
